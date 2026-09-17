7fe3e2afbc664f69af43c24206838d761c524a9e
mocao <mocaor2007@gmail.com>
2026-09-17 17:23:53 +0200

    En casa

--- DesarrolloDeInterfaces/src/ejercicios/Main.java
+++ DesarrolloDeInterfaces/src/ejercicios/Main.java
@@ -25,28 +25,43 @@ public class Main {
         };
 
 
-        int numPuntos = 0;
-        int numLineas = 0;
+        int cantidadNumeros = 0;
 
         try {
 
-            int cantidadNumeros  = MiEntradaSalida.leerEnteroRango("Introduce la cantidad de numeros que quieras pasar a morse: ", 1, 1000);
-        } catch (MiEntradaSalidaException e){
+            cantidadNumeros = MiEntradaSalida.leerEnteroRango("Introduce la cantidad de numeros que quieras pasar a morse: ", 1, 1000);
+        } catch (MiEntradaSalidaException e) {
             System.out.println(e.getMessage());
         }
 
 
-            for (int i = 0; i < num; i++) {
-                int num = MiEntradaSalida.leerEntero("Introduce tu numero: ");
-                for (int j = 0; j < morse.length; j++) {
-                    switch (morse.(j)){
+        for (int i = 0; i < cantidadNumeros; i++) {
+            String texto = MiEntradaSalida.leerLinea("Introduce tu numero: ");
+            int numPuntos = 0;
+            int numLineas = 0;
 
+            for (int j = 0; j < texto.length(); j++) {
+                char c = texto.charAt(j);
+                // Obtener el valor numérico del caracter (por ejemplo: '3' -> 3)
+                int digito = c - '0';
+
+                String representacion = morse[digito];
+
+                for (int k = 0; k < representacion.length(); k++) {
+                    char simbolo = representacion.charAt(k);
+                    if (simbolo == '.') {
+                        numPuntos++;
+                    } else if (simbolo == '-') {
+                        numLineas++;
                     }
+                }
 
             }
+            System.out.println(numPuntos + " " + numLineas);
         }
-    }
 
+
+    }
 
 
 }
